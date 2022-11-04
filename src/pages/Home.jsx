ea0b23f3032c6de1878d59550ea90dc6dee38716
Ben Tucker <bwtucker4@gmail.com>
2022-11-04 12:22:50 -0700

    Nav toggle changes on click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 				<div className="main-left"></div>
 				<div className="main-right">
 					<div className="hero">
-						<h1>We build designs for the future</h1>
+						<h1>We build <span className='blue'>designs</span> for the <span className='red'>future</span></h1>
 						<div className='learn-more'>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore corporis nulla, aliquam eum qui illo?</p>
 							<button>Learn More</button>
@@ -18,7 +18,7 @@ const Home = () => {
 					</div>
 				</div>
 			</main>
-			<button className='toggle-nav' onClick={() => setMenuOpen(prev => !prev)}>
+			<button className='toggle-nav' style={{ backgroundColor: menuOpen? "#d21700" : "#366c8c" }} onClick={() => setMenuOpen(prev => !prev)}>
 				<div className='hamburger'>
 					<span className={menuOpen ? "hamburger-bun top-bun active" : "hamburger-bun top-bun"}></span>
 					<span className={menuOpen ? "hamburger-bun bottom-bun active" : "hamburger-bun bottom-bun"}></span>
